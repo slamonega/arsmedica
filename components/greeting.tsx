0e52bcd5cbeb69cbee8a61390c7ad1fd33105238
slamonega <slamonega@gmail.com>
2025-04-28 21:57:07 -0300

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Ars Medica!
+        Ars Medica
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-       Excellence at the Frontier of Clinical Judgment
+     AI Support for Clinical Judgment
       </motion.div>
     </div>
   );
