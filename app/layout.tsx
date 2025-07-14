7648ce2e724833cc13015a2fafca5b3b35f61831
slamonega <slamonega@gmail.com>
2025-07-14 00:33:19 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://arsmedica.ai'),
+  title: 'Ars Medica',
+  description: 'Community Digital Health',
 };
 
 export const viewport = {
