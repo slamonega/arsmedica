8d1e0fe967a72ee31b48c5453753dc2e9f70d817
slamonega <slamonega@gmail.com>
2025-07-14 01:42:22 -0300

    Update greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-     AI Support for Clinical Judgment
+     Community Healthcare
       </motion.div>      
             <motion.div
         initial={{ opacity: 0, y: 10 }}
